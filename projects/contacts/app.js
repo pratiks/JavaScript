6767be6686d97a6e6516bf89f88ccc68bbf2de27
Pratik Shah <pshah@atlassian.com>
2017-04-26 15:23:34 -0500

    adding express framework under projects.

---
+++ projects/contacts/app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var env = require('node-env-file');
+
+var app = express();
+
+// if in development mode, load .env variables
+if (app.get("development")) {
+    env(__dirname + '/.env');
+}
+
+// connect to database
+//app.db = mongoose.connect(process.env.MONGODB_URI);
+
+// view engine setup - this app uses Hogan-Express
+// https://github.com/vol4ok/hogan-express
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.set('layout','layout');
+app.engine('html', require('hogan-express'));
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('json spaces', 2);
+var routes = require('./routes/index');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+module.exports = app;
---
+++ projects/contacts/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+// Contacts App
+
+
+
+// CRUD
+
+
+var contacts = [];
+
+//for loops, functions, API, arrays.
+
+
+module.exports = router;
